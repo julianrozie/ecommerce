c64fbbe85dc89d79acee3aee780a390256ca48ff
Julian Rozie <julianmr@gmail.com>
2023-08-25 02:13:21 -0700

    Add Orders Microservices

---
+++ ECommerce.Api.Orders/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerce.Api.Orders.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Orders.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrdersProvider ordersProvider;
+
+        public OrdersController(IOrdersProvider ordersProvider)
+        {
+            this.ordersProvider = ordersProvider;
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetOrderAsync(int customerId)
+        {
+            var result = await this.ordersProvider.GetOrdersAsync(customerId);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Orders);
+            }
+
+            return NotFound();
+        }
+    }
+}
